796cb7b038edf485971c72e69bc97cbd3d5bb78b
Lindsey Trainor <ltrainor38@gmail.com>
2017-11-26 02:21:05 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,6 +2,10 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
 
+document.addEventListener(keydown, onKeydown(input))
 
+}
 
+function onKeydown(input){
+  var index = 0
 }

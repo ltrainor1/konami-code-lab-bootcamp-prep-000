06d54245dccc1cc824d44c18d036ece7aec9b446
Lindsey Trainor <ltrainor38@gmail.com>
2017-11-26 02:50:37 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -8,7 +8,7 @@ document.addEventListener("keydown", function(e) {
   console.log(key)
   if (key === code[0]){
     index++;
-  
+  console.log(index)
   if (index === code.length){
     alert("Hurray!");
     index = 0;

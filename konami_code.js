65fb83fb66a01facd52fe7f00a2b29025d32509c
Lindsey Trainor <ltrainor38@gmail.com>
2017-11-26 02:39:13 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,12 +2,7 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
 
-document.addEventListener("keydown", onKeydown(), false)
-
-}
-
-function onKeydown(){
-  var index = 0
+document.addEventListener("keydown", (event) => )
   var e = event.key
   const key = parseInt(e.detail || e.which)
   if (key === code[index]){
